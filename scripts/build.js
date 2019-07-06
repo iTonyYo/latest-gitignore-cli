528172908e58ec8741af8fbec4fa998376251f4d
Tony Stark <swzyocowboy@icloud.com>
2019-07-07 00:15:59 +0800

    工作流: :railway_track: 更新 `yarn cli` 指令

--- scripts/build.js
+++ scripts/build.js
@@ -1,39 +1,53 @@
-import { resolve } from 'path';
-import { realpathSync } from 'fs';
+import { join, dirname } from 'path';
+
 import pMap from 'p-map';
 import fg from 'fast-glob';
 import execa from 'execa';
 import chalk from 'chalk';
 
-const resolveRoot = relativePath => resolve(
-  realpathSync(process.cwd()),
-  relativePath,
-);
+import { resolveCwd } from './paths';
+
+async function run() {
+  await pMap(await getSrcs(), async (src) => {
+    await build(src);
+    return;
+  }, { concurrency: 8 });
+
+  await execa('chmod', ['+x', resolveCwd('esm/cli.js')]);
+
+  console.log(chalk `{greenBright 构建成功!}`);
+}
+
+async function getSrcs() {
+  const srcs = await fg(['src/**/*.js']);
+  return srcs;
+}
+
+async function build(src) {
+  await execa(
+    'npx',
+    [
+      'babel',
+      '-d',
+      getDestPath(src),
+      resolveCwd(src),
+    ]
+  );
+}
+
+function getDestPath(path) {
+  return dirname(
+    join(
+      resolveCwd('esm'),
+      path.substring('src/'.length, path.length),
+    )
+  );
+}
 
 (async () => {
-  const src = fg(['src/**/*.js']);
-
-  await pMap(
-    await src,
-    async (path) => {
-      try {
-        return await execa(
-          'npx',
-          [
-            'babel',
-            '-d',
-            resolveRoot('esm'),
-            resolveRoot(path),
-          ]
-        );
-      } catch (err) {
-        throw err;
-      }
-    },
-    {concurrency: 8},
-  );
-
-  console.log(`
-    ${chalk.greenBright('构建成功!')}
-  `);
+  try {
+    await run();
+  } catch (err) {
+    throw(err);
+  }
 })();
